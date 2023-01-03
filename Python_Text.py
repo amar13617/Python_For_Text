c857b7b8ac7304b314cd6ce68eeab63dc09744a6
amar13617 <amar13617@gmail.com>
2023-01-03 15:28:55 +0530

    Reading text file in python

---
+++ Python_Text.py
@@ -0,0 +1,10 @@
+with open(r'C:\Users\lenovo\Dropbox\PC\Desktop\project.txt') as f:
+    lines = f.readlines()
+    #print(lines)
+    #print(lines[0])
+    
+#print(lines)
+
+for r in range(len(lines)):
+    #print(lines[r])
+    
